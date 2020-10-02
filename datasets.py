d73aa4d81f86c2e6af36693a0a0bb876651faddc
Albert <albert.bhagwan@gmail.com>
2020-10-02 17:30:54 +0200

    Added core 50 to datasets

--- datasets.py
+++ datasets.py
@@ -13,7 +13,9 @@ import torchvision.transforms as transforms
 from torchvision.datasets.utils import download_url, check_integrity
 import torch.utils.data as data
 from torch.utils.data import DataLoader
-         
+
+from data_loader import CORE50 as CORE50_base
+
 IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
 
 
@@ -105,14 +107,14 @@ class ImageFolder(data.Dataset):
   """
 
   def __init__(self, root, transform=None, target_transform=None,
-               loader=default_loader, load_in_mem=False, 
+               loader=default_loader, load_in_mem=False,
                index_filename='imagenet_imgs.npz', **kwargs):
     classes, class_to_idx = find_classes(root)
     # Load pre-computed image directory walk
     if os.path.exists(index_filename):
       print('Loading pre-saved Index file %s...' % index_filename)
       imgs = np.load(index_filename)['imgs']
-    # If first time, walk the folder directory and save the 
+    # If first time, walk the folder directory and save the
     # results to a pre-computed file.
     else:
       print('Generating  Index file %s...' % index_filename)
@@ -130,7 +132,7 @@ class ImageFolder(data.Dataset):
     self.target_transform = target_transform
     self.loader = loader
     self.load_in_mem = load_in_mem
-    
+
     if self.load_in_mem:
       print('Loading all images into memory...')
       self.data, self.labels = [], []
@@ -138,7 +140,7 @@ class ImageFolder(data.Dataset):
         path, target = imgs[index][0], imgs[index][1]
         self.data.append(self.transform(self.loader(path)))
         self.labels.append(target)
-          
+
 
   def __getitem__(self, index):
     """
@@ -156,10 +158,10 @@ class ImageFolder(data.Dataset):
       img = self.loader(str(path))
       if self.transform is not None:
         img = self.transform(img)
-    
+
     if self.target_transform is not None:
       target = self.target_transform(target)
-    
+
     # print(img.size(), target)
     return img, int(target)
 
@@ -175,7 +177,7 @@ class ImageFolder(data.Dataset):
     tmp = '    Target Transforms (if any): '
     fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
     return fmt_str
-        
+
 
 ''' ILSVRC_HDF5: A dataset to support I/O from an HDF5 to avoid
     having to load individual images all the time. '''
@@ -185,19 +187,19 @@ class ILSVRC_HDF5(data.Dataset):
   def __init__(self, root, transform=None, target_transform=None,
                load_in_mem=False, train=True,download=False, validate_seed=0,
                val_split=0, **kwargs): # last four are dummies
-      
+
     self.root = root
     self.num_imgs = len(h5.File(root, 'r')['labels'])
-    
+
     # self.transform = transform
-    self.target_transform = target_transform   
-    
+    self.target_transform = target_transform
+
     # Set the transform here
     self.transform = transform
-    
-    # load the entire dataset into memory? 
+
+    # load the entire dataset into memory?
     self.load_in_mem = load_in_mem
-    
+
     # If loading into memory, do so now
     if self.load_in_mem:
       print('Loading %s into memory...' % root)
@@ -217,22 +219,22 @@ class ILSVRC_HDF5(data.Dataset):
     if self.load_in_mem:
       img = self.data[index]
       target = self.labels[index]
-    
+
     # Else load it from disk
     else:
       with h5.File(self.root,'r') as f:
         img = f['imgs'][index]
         target = f['labels'][index]
-    
-   
+
+
     # if self.transform is not None:
         # img = self.transform(img)
     # Apply my own transform
     img = ((torch.from_numpy(img).float() / 255) - 0.5) * 2
-    
+
     if self.target_transform is not None:
       target = self.target_transform(target)
-    
+
     return img, int(target)
 
   def __len__(self):
@@ -259,7 +261,7 @@ class CIFAR10(dset.CIFAR10):
       raise RuntimeError('Dataset not found or corrupted.' +
                            ' You can use download=True to download it')
 
-    # now load the picked numpy arrays    
+    # now load the picked numpy arrays
     self.data = []
     self.labels= []
     for fentry in self.train_list:
@@ -276,34 +278,34 @@ class CIFAR10(dset.CIFAR10):
       else:
         self.labels += entry['fine_labels']
       fo.close()
-        
+
     self.data = np.concatenate(self.data)
     # Randomly select indices for validation
     if self.val_split > 0:
       label_indices = [[] for _ in range(max(self.labels)+1)]
       for i,l in enumerate(self.labels):
-        label_indices[l] += [i]  
+        label_indices[l] += [i]
       label_indices = np.asarray(label_indices)
-      
+
       # randomly grab 500 elements of each class
       np.random.seed(validate_seed)
-      self.val_indices = []           
+      self.val_indices = []
       for l_i in label_indices:
         self.val_indices += list(l_i[np.random.choice(len(l_i), int(len(self.data) * val_split) // (max(self.labels) + 1) ,replace=False)])
-    
-    if self.train=='validate':    
+
+    if self.train=='validate':
       self.data = self.data[self.val_indices]
       self.labels = list(np.asarray(self.labels)[self.val_indices])
-      
+
       self.data = self.data.reshape((int(50e3 * self.val_split), 3, 32, 32))
       self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
-    
+
     elif self.train:
       print(np.shape(self.data))
       if self.val_split > 0:
         self.data = np.delete(self.data,self.val_indices,axis=0)
         self.labels = list(np.delete(np.asarray(self.labels),self.val_indices,axis=0))
-          
+
       self.data = self.data.reshape((int(50e3 * (1.-self.val_split)), 3, 32, 32))
       self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
     else:
@@ -322,7 +324,7 @@ class CIFAR10(dset.CIFAR10):
       fo.close()
       self.data = self.data.reshape((10000, 3, 32, 32))
       self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
-      
+
   def __getitem__(self, index):
     """
     Args:
@@ -343,7 +345,7 @@ class CIFAR10(dset.CIFAR10):
       target = self.target_transform(target)
 
     return img, target
-      
+
   def __len__(self):
       return len(self.data)
 
@@ -360,3 +362,7 @@ class CIFAR100(CIFAR10):
     test_list = [
         ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
     ]
+
+class CORE50(CORE50_base):
+    root='/home/abhagwan/datasets/core50'
+    scenario="nicv2_391"
